669038c0c4e4ee4d925e71a0bbd71f45d1f71921
marnie <marniejstaples@gmail.com>
2022-12-18 12:55:01 -0800

    add math.round function to backing calculation

--- quilt-calculator/script.js
+++ quilt-calculator/script.js
@@ -8,23 +8,23 @@ function returnBacking() {
     userWidth = document.getElementById("userwidth").value;
     userLength = document.getElementById("userlength").value;
 
-    if (userWidth < 35 && userLength < 35) {
-        backing = (parseInt(userWidth) + parseInt(userLength)) / 36;
+    if (userWidth <= 34 && userLength <= 34) {
+        backing = Math.round(((parseInt(userWidth) + parseInt(userLength)) / 36) * 10) / 10;
         document.getElementById("backing").innerHTML = backing;
     }
 
-    else if (userWidth >= 40 && userWidth <= 60) {
-        backing = ((userWidth * 2) + 12) / 36;
+    else if (userWidth >= 35 && userWidth <= 60) {
+        backing = Math.round((((parseInt(userWidth) * 2) + 12) / 36) * 10) / 10;
         document.getElementById("backing").innerHTML = backing;
     }
 
     else if (userWidth >= 61 && userWidth <= 80) {
-        backing = ((userLength * 2) + 12) / 36;
+        backing = Math.round((((userLength * 2) + 12) / 36) * 10) / 10;
         document.getElementById("backing").innerHTML = backing;
     }
 
     else if (userWidth >= 81 && userWidth <= 120) {
-        backing = ((userLength * 3) + 18) / 36;
+        backing = Math.round((((userLength * 3) + 18) / 36) * 10) / 10;
         document.getElementById("backing").innerHTML = backing;
     }
 }
